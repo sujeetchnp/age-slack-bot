300a704803ac2ec4ef45630081c6be8a3f64786c
sujeetchnp <sujeetchnp@gmail.com>
2023-03-29 00:25:45 -0400

    Fixed

--- main.go
+++ main.go
@@ -17,13 +17,14 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 		fmt.Println(event.Command)
 		fmt.Println(event.Parameters)
 		fmt.Println(event.Event)
+		fmt.Println(event.Event.UserID)
 		fmt.Println()
 	}
 }
 
 func main() {
 	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5025531168226-5049247463472-xPEDi5i0a8Q34FSKY6EenCGo")
-	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A050RGAHV7U-5018946953494-7f51a636c618895b3cbf9b521b93ca46061b5a21e71bf565c6d993330d185c05")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A050RGAHV7U-5038469306977-de3a70b7d73d2b4f0e99fda4542cea7c6fcea801ec04c2972d7be9a05d42d853")
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
@@ -31,7 +32,7 @@ func main() {
 
 	bot.Command("my yob is <year>", &slacker.CommandDefinition{
 		Description: "yob calculator",
-		//Example:     "my yob is 2020",
+		Examples:    []string{"my yob is 1990"},
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			year := request.Param("year")
 			yob, err := strconv.Atoi(year)
